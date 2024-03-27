f5a8291804f3a97debcfcd50d47374c6a8fb360f
samiullahNarejo58 <57575943+samiullahNarejo58@users.noreply.github.com>
2024-03-27 15:53:52 +0500

    correction second time

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:safdar8455/safdar8455.github.io.git main
+git push -f git@github.com:samiullahNarejo58/samiullahNarejo58.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:safdar8455/Portfolio.git main:gh-pages
+# git push -f git@github.com:samiullahNarejo58/Narejo.git main:gh-pages
 
 cd -
